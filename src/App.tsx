d64b729330c741cffe6f30d572387e3a6ba7bd74
Yuri Boblak <sseslesnes@gmail.com>
2025-09-08 18:25:10 +0300

    antProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 // import { useState } from "react";
+// import { Children } from "react";
 import "./App.css";
+
+interface AntProps {
+  x: number;
+  y: number;
+}
 
 const gridSize = [40, 30]; //columns y, row x
 const gridData = Array.from({ length: gridSize[0] }, () =>
   Array.from({ length: gridSize[1] }, () => 0)
 );
 
-gridData[1][2] = 1; //x,y
+// array center coords
+const antProps = {
+  x: Math.round(gridSize[0] / 2),
+  y: Math.round(gridSize[1] / 2),
+};
+
+function ant(props: AntProps) {
+  // На білому квадраті — повернути на 90° праворуч, змінити колір квадрата на чорний, зробити крок уперед наступну клітину.
+  if (gridData[props.x][props.y] === 0) {
+    console.log("0");
+  }
+
+  // На чорному квадраті - повернути на 90 ° вліво, змінити колір квадрата на білий, зробити крок уперед на наступну клітину.
+  if (gridData[props.x][props.y] === 1) {
+    console.log("1");
+  }
+}
+// direction 0 up, 1 right, 2 down, 3 left
 
 function Cell({ value }: { value: number }) {
   const cellState = value ? "cell color-set" : "cell color-unset";
@@ -26,3 +49,5 @@ export default function App() {
     </div>
   );
 }
+
+ant(antProps);
